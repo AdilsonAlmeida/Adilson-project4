74a2ef47ae93e352b78f62ab41daa613395e3d30
AdilsonAlmeida <adilson@adilsonalmeida.com>
2014-03-27 21:57:00 -0500

    added 1st function to the repo.

--- DosReisEAlmeida-Adilson.js
+++ DosReisEAlmeida-Adilson.js
@@ -5,3 +5,32 @@
 // Project 4
 // Problem Solving Functions
 
+//global variables
+var stringReturn
+//String Function 1
+
+
+
+var getpattern =function(argGetpattern) {
+
+	var stringLength = argGetpattern.length;
+	
+		for (var i = 11; i = stringLength; i) {
+		
+			if (argGetpattern.substring(0,2) < 1000) {
+			
+				if (argGetpattern.charAt(3) = "-") {
+				
+					if (argGetpattern.charAt(7) = "-") {
+					
+					return true};
+				};
+			};
+		};
+		return false
+} 
+
+
+
+//main code with function calls and output
+stringReturn = argGetpattern("123-456-7890")
